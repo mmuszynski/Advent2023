2465367db550e15b3464aad6fab1b431690055e9
Mike Muszynski <mmuszynski@gmail.com>
2023-12-04 12:04:39 -0500

    Initial Commit

---
+++ Advent2023/Advent2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Advent2023App.swift
+//  Advent2023
+//
+//  Created by Mike Muszynski on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Advent2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Advent2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Advent2023
+//
+//  Created by Mike Muszynski on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
